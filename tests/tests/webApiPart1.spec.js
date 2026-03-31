c682113ae1abdb1062d978842a6d0ea7ac7efb35
Malith212 <malithnavindu130@gmail.com>
2026-03-31 16:22:11 +0530

    Refactor webApiPart1 test: streamline login process and remove unnecessary logs

--- tests/tests/webApiPart1.spec.js
+++ tests/tests/webApiPart1.spec.js
@@ -1,46 +1,32 @@
 import { test, expect, request } from '@playwright/test';
 const loginPayload = {userEmail: "navindumalith0@gmail.com", userPassword: "Malith123@"};
+let token;
 
 test.beforeAll(async () =>{
-    console.log("Before all tests");
-
     const apiContext = await request.newContext();
     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {
         data: loginPayload
     });
-    expect(loginResponse.ok()).toBeTruthy();
-    
+
     const loginResponseJson = await loginResponse.json();
-    const token = loginResponseJson.token;
-
-    console.log(token);
+    token = loginResponseJson.token;
 });
 
+test('Open Web API Part 1', async ({ browser }) => {
+    const context = await browser.newContext();
 
-test.beforeEach(() =>{
-    
-});
+    await context.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token);
 
-test.only('Open Web API Part 1', async ({ page }) => { 
-    await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+    const page = await context.newPage();
 
-    const username = page.locator('#userEmail');
-    const password = page.locator("[type='password']");  
-    const logIn = page.locator("#login");
+    await page.goto('https://rahulshettyacademy.com/client');
 
-    await username.fill("navindumalith0@gmail.com");
-    await password.fill("Malith123@");
-    await logIn.click();
+    await page.waitForTimeout(5000);
 
-    await page.waitForTimeout(10000);
-
-    const cardTitle = await page.locator(".card-body b");
-    
-    // await page.waitForLoadState('networkidle');//flicky
-
+    const cardTitle = page.locator(".card-body b");
     await cardTitle.first().waitFor();
 
-    const allTitles = await cardTitle.allTextContents();
-
-    console.log(allTitles);
+    console.log(await cardTitle.allTextContents());
 });
